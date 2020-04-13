6232ee41620e5ab2ebbd72ac026d960692988410
Jordan Nunez <jmnunez@wisc.edu>
2020-04-13 14:27:59 -0500

    angryjamie

--- angry.js
+++ angry.js
@@ -11,7 +11,7 @@ function test() {
   
     
 
-    let ctl = new T.CubeTextureLoader().load(['madjordan.png', 'madkayla.png', 'madalyssa.png', 'madgabe.png', 'madjordan.png', 'madkayla.png']);
+    let ctl = new T.CubeTextureLoader().load(['angryjamie.png', 'madkayla.png', 'madalyssa.png', 'madgabe.png', 'madjordan.png', 'madkayla.png']);
     world.scene.background = ctl;
 
     let sphereGeom = new T.SphereGeometry(3.5, 50, 50);
